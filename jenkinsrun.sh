420aae6d559a7fcd2d8af794ccca5d2da4aefea7
simon hewitt <simon.d.hewitt@gmail.com>
2019-05-15 13:10:55 +0100

    Add initial Jenkinsfile

---
+++ jenkinsrun.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+docker run \
+  --rm \
+  -u root \
+  -p 8082:8080 \
+  -v jenkins-data:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$HOME":/home \
+  jenkinsci/blueocean
